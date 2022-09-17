124c67d191950398dbffced8353669d7966053cd
Thae Oo Nge <aprilthae1998@gmail.com>
2022-09-17 23:23:46 +0630

    semantic-ui

--- src/App.js
+++ src/App.js
@@ -3,9 +3,24 @@
 function App() {
   return (
     <div >
-      <h3>My name is ThaeOo Nge</h3>
-      <h5>Welcome to React Js</h5>
-    </div>
+     <div className=" ui container comments">
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar"/>
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            Thae
+          </a>
+          <div className="metadata">
+            <span className="date">Today is 17 Sep 2022.</span>
+          </div>
+          <div className="text">Nice comment</div>
+        </div>
+      </div>
+     </div>
+    
+     </div>
   );
 }
 
