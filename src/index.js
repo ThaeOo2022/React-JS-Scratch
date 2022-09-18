2e97bb3150dfe1a7e314dd25ea0fa53e8af84d46
Thae Oo Nge <aprilthae1998@gmail.com>
2022-09-18 22:28:22 +0630

    passing multiplte props

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import CommentDetail from './CommentDetail';
+import {faker} from '@faker-js/faker';
 
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const App = () => {
+  return (
+    <div >
+      <div className=" ui container comments">
+        <CommentDetail author="Thae" timeAgo="Today at 7 am" pimg={faker.image.avatar()}/>
+        <CommentDetail author="Oo" timeAgo="Today at 5 am" pimg={faker.image.avatar()}/>
+        <CommentDetail author="Nge" timeAgo="Today at 9 am" pimg={faker.image.avatar()}/>
+      </div>
+
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -14,4 +27,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//reportWebVitals();
