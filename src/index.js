69d9fe06b9b62a33d65c438bf26c18ee205f9ba9
Thae Oo Nge <aprilthae1998@gmail.com>
2022-09-25 11:18:54 +0630

    State in react component

--- src/index.js
+++ src/index.js
@@ -1,23 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const geofun=()=>{
-  window.navigator.geolocation.getCurrentPosition(
-    (position)=>console.log(position),
-    (err)=>console.log(err)
-  );
-}
 
-class App extends React.Component{
-  render(){
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { lat: null, errorMessage: null };
+
+
+
+  }
+
+  render() {
+
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({ lat: position.coords.latitude});
+     
+      },
+      (err) => {this.setState({errorMessage:err.message})}
+    );
+     if(this.state.errorMessage && ! this.state.lat ){
+      return <div>Error:{this.state.errorMessage}</div>
+     }
+     if(!this.state.errorMessage &&  this.state.lat ){
+      return <div>Latitude:{this.state.lat}</div>
+     }
     
 
-geofun();
-
-  return(
-    <div>Latitude:</div>
-  );
-}
+   
+  }
 }
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
