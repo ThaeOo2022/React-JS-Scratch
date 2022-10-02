2faaac13db7e7d72d6fc711916c7718d992d35ca
Thae Oo Nge <aprilthae1998@gmail.com>
2022-10-02 22:11:43 +0630

    Tenary Expression in JSX

--- src/SeasonDisp.js
+++ src/SeasonDisp.js
@@ -5,16 +5,17 @@ function SeasonDisp(props) {
   const getSeason = (lat, month) => {
     
       if (month > 2 && month < 9) {
-        return lat > 0 ? "Summer" : "Winter";
+        return lat > 0 ? "summer" : "winter";
       } else {
         return lat > 0 ? "winter" : "summer";
       }
     }
     const season=getSeason(props.lat, new Date().getMonth());
+    //const seasonDescription= season === 'winter' ? 'Burr it is Chilly' : 'Let hit the beach';
     return (
       <div>
         
-        Season : {season}
+        Season : {season === 'winter' ? 'Burr it is Chilly' : 'Let hit the beach'}
       </div>
     )
   }
