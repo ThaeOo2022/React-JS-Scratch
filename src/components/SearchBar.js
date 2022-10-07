0d47eed0cb88a42795f164df45443f514205f12a
Thae Oo Nge <aprilthae1998@gmail.com>
2022-10-07 22:54:42 +0630

    Showing form to the user

---
+++ src/components/SearchBar.js
@@ -0,0 +1,17 @@
+import React from "react";
+class SearchBar extends React.Component{
+
+render(){
+    return(
+        <div>
+            <from>
+                <input type="text"></input>
+            </from>
+        </div>
+    );
+}
+
+}
+
+
+export default SearchBar;
