0b4535112ae3fd51fdf5c16dea1dea220e86f6c3
Thae Oo Nge <aprilthae1998@gmail.com>
2022-10-10 21:24:00 +0630

    Handling Form Submitted

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,20 +1,21 @@
 import React from "react";
 class SearchBar extends React.Component {
-    onInputChange(event) {
-        console.log(event.target.value);
+    state = {
+        term: "Hi there"
     }
-    onInputClick() {
-        console.log("Input was clicked .....");
+    onSubmitChange(event){
+        event.preventDefault();
+        console.log("from submit event");
     }
     render() {
         return (
             <div className="ui segment">
-                <form className="ui form">
+                <form onSubmit={this.onSubmitChange} className="ui form">
                     <div className="field">
                         <label>Search Image</label>
                         <input type="text"
-                            onClick={this.onInputClick}
-                            onChange={this.onInputChange}></input>
+                            value={this.state.term}
+                            onChange={e => this.setState({ term: e.target.value })}></input>
                     </div>
 
                 </form>
