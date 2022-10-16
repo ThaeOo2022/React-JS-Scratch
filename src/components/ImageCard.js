22ecbb2861baf3efbaa54fd0a7887f2d83bddca1
Thae Oo Nge <aprilthae1998@gmail.com>
2022-10-16 22:39:58 +0630

    Callback on image load

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,16 +1,66 @@
 import React from "react";
-class ImageCard extends React.Component{
-render(){
-    const {description,urls}=this.props.image;
-    return(
-        <>
-        <img 
-            alt={description}
-            src={urls.regular}
-        />
-        </>
-    );
-}
+class ImageCard extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.imageRef = React.createRef();
+    }
+
+  //  componentDidMount() {
+        //console.log(this.imageref);
+     //   this.imageref.current.addEventListener('load', this.setSpans);
+  //  }
+  componentDidMount() {
+    console.log(this.imageRef.current.clientHeight);
+   this.imageRef.current.addEventListener('load', this.setSpans);
+  }
+
+    setSpans = () => {
+        console.log(this.imageRef.current.clientHeight);
+    }
+    render() {
+        const { description, urls } = this.props.image;
+        return (
+            <>
+                <img
+                    ref={this.imageRef}
+                    alt={description}
+                    src={urls.regular}
+                />
+            </>
+        );
+    }
 }
 
 export default ImageCard;
+
+
+/*import React from 'react';
+
+class ImageCard extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.imageRef = React.createRef();
+  }
+
+  componentDidMount() {
+    this.imageRef.current.addEventListener('load', this.setSpans);
+  }
+
+  setSpans = () => {
+    console.log(this.imageRef.current.clientHeight);
+  };
+
+  render() {
+    const { description, urls } = this.props.image;
+
+    return (
+      <div>
+        <img ref={this.imageRef} alt={description} src={urls.regular} />
+      </div>
+    );
+  }
+}
+
+export default ImageCard;*/
