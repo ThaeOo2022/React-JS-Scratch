feda6c27fa9093f2963ca4da8796fb3f6a640d35
Thae Oo Nge <aprilthae1998@gmail.com>
2022-10-28 17:09:41 +0630

    Building and Styling the Accordion

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,8 +1,23 @@
+import React from 'react'
+const Accordion = ({ items }) => {
+    const renderitems = items.map((item) => {
+        return (
+            <React.Fragment key={item.title}>
+                <div className="title active">
+                    <i className="dropdown icon"></i>
+                    {item.title}
+                </div>
+                <div className="contect active">
+                    <p>{item.content}</p>
 
-export default ({items})=>{
-    return(
-        <div>
-            {items.length}
+                </div>
+            </React.Fragment>
+        );
+    });
+    return (
+        <div className="ui styled accordion">
+            {renderitems}
         </div>
     );
 }
+export default Accordion;
