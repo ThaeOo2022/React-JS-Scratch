4acac06a6e3507ad68edc0d99e167aafeedc3c5d
Thae Oo Nge <aprilthae1998@gmail.com>
2022-10-30 20:23:29 +0630

    List Building!

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import Accordion from "./components/Accordion";
+import Search from "./components/Search";
 
-export default  ()=>{
-    const items=[
+export default () => {
+    const items = [
         {
-            title:"What is React?",
-            content:"React is a font end javascript library"
+            title: "What is React?",
+            content: "React is a font end javascript library"
         },
         {
-            title:"What is React1?",
-            content:"React is a font end javascript library"
+            title: "What is React1?",
+            content: "React is a font end javascript library"
         },
         {
-            title:"What is React2?",
-            content:"React is a font end javascript library"
+            title: "What is React2?",
+            content: "React is a font end javascript library"
         }
     ]
     return (
         <div>
-           
-            <Accordion items={items}/>
+
+            <Search />
         </div>
     );
 }
---
+++ src/components/Search.js
@@ -0,0 +1,55 @@
+import { useEffect, useState } from "react";
+import axios from 'axios';
+
+const Search = () => {
+    const [term, setTerm] = useState('programming');
+    const [results,setResult]=useState([]);
+
+    useEffect(()=>{
+       const search=async ()=>{
+        const {data}=await axios.get('https://en.wikipedia.org/w/api.php',{
+            params:{
+                action:'query',
+                list:'search',
+                origin:'*',
+                format:'json',
+                srsearch:term,
+            }
+        });
+        setResult(data.query.search);
+       }
+       search();
+    },[term]);
+
+    const renderedList=results.map(result=>{
+        return(
+            
+                <div key={result.pageid} className="item">
+                    <div className="content">
+                        <div className="header">
+                            {result.title}
+                        </div>
+                        {result.snippet}
+                    </div>
+                </div>
+          
+        );
+    });
+
+    return (
+        <div>
+            <div className="ui form">
+                <div className="field">
+                    <label>Enter search Term</label>
+                    <input
+                        value={term}
+                        onChange={e => setTerm(e.target.value)}
+                        className="input" />
+                        {console.log(results)}
+                </div>
+            </div>
+            <div className="ui celled list">{renderedList}</div>
+        </div>
+    );
+}
+export default Search;
