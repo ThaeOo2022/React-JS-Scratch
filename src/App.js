f7a73f7a8052508a61826c02a433a3d6d313e1c9
Thae Oo Nge <aprilthae1998@gmail.com>
2022-11-06 00:06:49 +0630

    Selection state

--- src/App.js
+++ src/App.js
@@ -1,40 +1,48 @@
+import { useState } from "react";
 import Accordion from "./components/Accordion";
 import Dropdown from "./components/Dropdown";
 import Search from "./components/Search";
 
+const options = [
+    {
+        label: "The color red",
+        value: 'red'
+    },
+    {
+        label: "The color green",
+        value: 'green'
+    },
+    {
+        label: "The color blue",
+        value: 'blue'
+    }
+];
+const items = [
+    {
+        title: "What is React?",
+        content: "React is a font end javascript library"
+    },
+    {
+        title: "What is React1?",
+        content: "React is a font end javascript library"
+    },
+    {
+        title: "What is React2?",
+        content: "React is a font end javascript library"
+    }
+];
 export default () => {
-    const options=[
-        {
-            label:"The color red",
-            value:'red'
-        },
-        {
-            label:"The color green",
-            value:'green'
-        },
-        {
-            label:"The color blue",
-            value:'blue'
-        }
-    ]
-    const items = [
-        {
-            title: "What is React?",
-            content: "React is a font end javascript library"
-        },
-        {
-            title: "What is React1?",
-            content: "React is a font end javascript library"
-        },
-        {
-            title: "What is React2?",
-            content: "React is a font end javascript library"
-        }
-    ]
+    const [selected, setSelected] = useState(options[0]);
+
     return (
         <div>
-            <Dropdown options={options}/>
-          
+            <Dropdown
+                
+                options={options}
+                selected={selected}
+                onSelectedChange={setSelected}
+            />
+
         </div>
     );
 }
